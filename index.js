4cbdd4a4ad416ae061d2e0db1917f0cf443007e3
PerryCR7 <Parvinderkc123@gmail.com>
2021-06-02 11:24:52 +0530

    files updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const express = require('express')
 const app = express()
+const Port = process.env.PORT || 3000
 const hbs = require('hbs')
 const request = require('request')
 console.log(__dirname)
@@ -77,6 +78,6 @@ app.get('*', (req, res) => {
    // res.send('My 404 Page!')
    res.render('index')
 })
- app.listen(3000, () => {
+ app.listen(Port, () => {
      console.log('Server is up on port!')
  })
