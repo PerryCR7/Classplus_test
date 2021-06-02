660c8748a8a55438a15a5a97940d6050fc63dfe7
PerryCR7 <Parvinderkc123@gmail.com>
2021-06-02 12:19:03 +0530

    make it better

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-const port = Process.env.PORT || 3000 ;
+// const port = Process.env.PORT || 3000 
+const port =  3000 
 const hbs = require('hbs')
 
 
